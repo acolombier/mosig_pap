100e51c4a4bffd3d6597481a9801529a285a34bf
Antoine <antoine38100@gmail.com>
2018-03-31 18:46:49 +0200

    Refactoring lab6

--- lab6.c
+++ lab6.c
@@ -8,62 +8,71 @@
 
 int main(int argc, char *argv[])
 {
-  int my_rank,my_newrank;
-  int size, dims[2], period[2] = {1, 0}, reorder = 1, coord[2];
-  
-  int matA[4][4] = {{0, 1, 1, 2},
+    int main_rank, cart_rank, row_rank;
+    int size, dims[2], period[2] = {1, 0}, reorder = 1, coord[2];
+
+    int matA[4][4] = {{0, 1, 1, 2},
                     {0, 1, 1, 2},
                     {0, 1, 1, 2},
                     {0, 1, 1, 2}};
                     
-  int matB[4][4] = {{0, 1, 1, 2},
+    int matB[4][4] = {{0, 1, 1, 2},
                     {0, 1, 1, 2},
                     {0, 1, 1, 2},
                     {0, 1, 1, 2}};
-  int matC[4][4]= {{0}};
-  
-  //~ MPI_Comm newcomm, oldcomm;
-  MPI_Comm comm, subcomm;
-  MPI_Group group;
-
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  
-  int q = floor(sqrt(size));
-  //~ printf(" the sqrt is %d\n", q );
-  dims[0] = dims[1] = q;
-  
-  int nodes = q * q;
-
-  MPI_Dims_create( nodes, 2, dims );
-  MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, reorder, &comm);
-  MPI_Comm_rank(comm, &my_newrank);
-  
-  MPI_Cart_coords(comm, my_newrank, 2, coord);
-  
-  int remain_dims[2] = {1, 0};
-  MPI_Cart_sub(comm, remain_dims, &subcomm);
-  
-  for (int k = 0; k < q; k++){
-    if (my_rank % 4 == (my_rank / 4 + k) % 4) 
-        MPI_Bcast(matA[my_rank / 4] + ((my_rank / 4 + k) % 4), 1 , MPI_INT, ((my_rank / 4 + k) % 4), subcomm); //everybody needs to send my_rank, matA[my_rank / 4][my_rank % 4], matB[my_rank / 4][my_rank % 4], my_newrank, coord[0],coord[1]);
-    matC[my_rank / 4][my_rank % 4] = matA[my_rank / 4][my_rank % 4] * matC[my_rank / 4][my_rank % 4];
+    int matC[4][4]= {{0}};
     
-    int rank_nightboor[2];
-    MPI_Cart_shift(comm, 0, 1, rank_nightboor, rank_nightboor+1);
-    //~ printf("My rank is %d, up is %d and down is %d, transmitting %d\n", my_rank, rank_nightboor[0], rank_nightboor[1], matB[my_rank / 4][my_rank % 4]);
-    MPI_Status status;
-    MPI_Sendrecv(matB[my_rank / 4] + (my_rank % 4), sizeof(int), MPI_INT,
-            rank_nightboor[0], 0,
-            matB[my_rank / 4] + (my_rank % 4), sizeof(int), MPI_INT,
-            rank_nightboor[1], 0,
-            MPI_COMM_WORLD, &status);
+    // Initialise MPI
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Retrieve the MPI constants
+    MPI_Comm cart_comm, row_comm;
+    MPI_Comm_rank(MPI_COMM_WORLD, &main_rank);
+
+    int q = floor(sqrt(size));
     
-    printf("K=%d result to=%d for %d\n", k, status.MPI_ERROR, my_rank);
-  }
+    // Create the cartesian
+    //~ MPI_Dims_create(size, 2, dims);
+    dims[0] = dims[1] = q;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, reorder, &cart_comm);
+    MPI_Comm_rank(cart_comm, &cart_rank);
+    MPI_Cart_coords(cart_comm, cart_rank, 2, coord);
 
-  //~ printf("Hello, world. My rank is %d, and this I have A = %d and B = %d with coordinates (%d %d)\n",
+    // Create rows 
+    int remain_dims[2] = {1, 0};
+    MPI_Cart_sub(cart_comm, remain_dims, &row_comm); 
+    MPI_Comm_rank(row_comm, &row_rank);
 
-  MPI_Finalize();
+    //~ printf("Rank %d, Cart_rank %d, Row_rank %d, PID %d\n", main_rank, cart_rank, row_rank, getpid());
+
+    for (int k = 0; k < 3; k++){
+        //~ if (k == 3)
+
+        if (row_rank == k)
+            MPI_Bcast(matA[cart_rank / 4] + ((cart_rank / 4 + k) % 4), 1, MPI_INT, (cart_rank / 4 + k) % 4, row_comm); //everybody needs to send my_rank, matA[my_rank / 4][my_rank % 4], matB[my_rank / 4][my_rank % 4], row_rank, coord[0],coord[1]);
+            
+        printf("rank=%d, col=%d, root=%d\n", cart_rank, row_rank, (row_rank + k) % 4);
+         
+        //~ // Computation
+        //~ matC[my_rank / 4][my_rank % 4] += matA[my_rank / 4][my_rank % 4] * matB[my_rank / 4][my_rank % 4];
+
+        //~ int rank_nightboor[2];
+        //~ MPI_Cart_shift(cart_comm, 0, 1, rank_nightboor, rank_nightboor+1);
+        //~ printf("My rank is %d, up is %d and down is %d, transmitting %d\n", my_rank, rank_nightboor[0], rank_nightboor[1], matB[my_rank / 4][my_rank % 4]);
+        //~ MPI_Status status;
+
+        //~ MPI_Sendrecv_replace(matB[my_rank / 4] + (my_rank % 4), 1, MPI_INT,
+                             //~ rank_nightboor[0], k, rank_nightboor[1], k,
+                             //~ cart_comm, &status);
+        //~ if (k == 1) 
+            //~ printf("DONE: K=%d from=%d: recv %d, send %d\n", k, my_rank, rank_nightboor[0], rank_nightboor[1]);
+
+        //~ if (k == 1) printf("K=%d result to=%d for %d\n", k, status.MPI_ERROR, my_rank);
+    }
+
+    //~ printf("Hello, world. My rank is %d, and this I have A = %d and B = %d with coordinates (%d %d)\n",
+
+    MPI_Finalize();
 }
+
